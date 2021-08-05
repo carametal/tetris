a5dea470ce6f529d3148383818dd82b31230eeba
carametal <mkatsdevelop@gmail.com>
2021-08-05 22:42:20 +0900

    Fieldの実装

--- src/components/Field.vue
+++ src/components/Field.vue
@@ -2,14 +2,8 @@
 <div class="wrapper">
   <table>
     <tbody>
-      <tr
-        v-for="(_, i) in Array(height).fill(0)"
-        :key="i"
-      >
-        <td
-          v-for="(_, j) in Array(width).fill(0)"
-          :key="j"
-        ></td>
+      <tr v-for="(row,i ) in field" :key="i">
+        <td v-for="(col, j) in row" :key="j"></td>
       </tr>
     </tbody>
   </table>
@@ -27,6 +21,19 @@ export default Vue.extend({
     width: {
       type: Number,
       required: true,
+    }
+  },
+  data() {
+    return {
+      field: [] as boolean[][]
+    }
+  },
+  created() {
+    this.init()
+  },
+  methods: {
+    init() {
+      this.field = Array(this.width).fill(Array(this.width).fill(false))
     }
   },
 })
