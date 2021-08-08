e9ac240af05c6d51c743506f853cfd42b3e30a3b
carametal <mkatsdevelop@gmail.com>
2021-08-08 13:01:35 +0900

    テトリミノの初期描画を仮実装

--- src/components/Field.vue
+++ src/components/Field.vue
@@ -2,10 +2,11 @@
 <div class="wrapper">
   <table>
     <tbody>
-      <tr v-for="(_ ,i) in height" :key="i">
+      <tr v-for="(_ ,x) in height" :key="x">
         <td
-          v-for="(_, j) in width"
-          :key="j"
+          v-for="(_, y) in width"
+          :key="y"
+          :style="{ 'background-color': getColor(x, y)}"
         ></td>
       </tr>
     </tbody>
@@ -21,6 +22,10 @@ interface Block {
   y: number
 }
 
+interface Tetrimino {
+  blocks: Block[]
+}
+
 export default Vue.extend({
   props: {
     height: {
@@ -34,9 +39,37 @@ export default Vue.extend({
   },
   data() {
     return {
+      tetriminos: [] as Tetrimino[],
     }
   },
+  created(): void {
+    this.addTetrimino()
+  },
   methods: {
+    addTetrimino(): void {
+      const blocks: Block[] = [
+        { x: 0, y: 5 },
+        { x: 0, y: 6 },
+        { x: 1, y: 5 },
+        { x: 1, y: 6 },
+      ]
+      const tetrimino = { blocks: blocks } as Tetrimino
+      this.tetriminos.push(tetrimino)
+      console.log(this.tetriminos)
+    },
+    hasBlock(x: number, y: number): boolean {
+      const ret = this.tetriminos.some(t => {
+        return t.blocks.some(b => b.x === x && b.y === y)
+      })
+      console.log(x, y, ret)
+      return ret
+    },
+    getColor(x: number, y: number): string {
+      if (this.hasBlock(x, y)) {
+        return 'black'
+      }
+      return 'white'
+    }
   },
 })
 </script>
