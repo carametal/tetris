553bb2684c30d79ac8e14e7c9fc8827fd2a7a9cb
carametal <mkatsdevelop@gmail.com>
2021-08-09 11:58:27 +0900

    矢印キーでテトリミノを移動させる機能を実装

--- src/components/Field.vue
+++ src/components/Field.vue
@@ -2,10 +2,10 @@
 <div class="wrapper">
   <table>
     <tbody>
-      <tr v-for="(_ ,x) in height" :key="x">
+      <tr v-for="(_ ,y) in height" :key="y">
         <td
-          v-for="(_, y) in width"
-          :key="y"
+          v-for="(_, x) in width"
+          :key="x"
           :style="{ 'background-color': getColor(x, y)}"
         ></td>
       </tr>
@@ -17,13 +17,76 @@
 <script lang="ts">
 import Vue from 'vue'
 
-interface Block {
+class Point {
   x: number
   y: number
+
+  constructor(x: number, y: number) {
+    this.x = x
+    this.y = y
+  }
 }
 
-interface Tetrimino {
+class Block {
+  point: Point
+
+  constructor(point: Point) {
+    this.point = point
+  }
+}
+
+interface ITetrimino {
   blocks: Block[]
+}
+
+class Tetrimino {
+  blocks: Block []
+
+  constructor(blocks: Block[]) {
+    this.blocks = blocks
+  }
+
+  public getCenter(): Point {
+    return this.blocks[0].point
+  }
+
+  public hasBlock(x: number, y: number): boolean {
+    return this.blocks.some(b => b.point.x === x && b.point.y === y)
+  }
+
+  public right(): void {
+    this.blocks = this.blocks.map(b => {
+      b.point.x++
+      return b
+    })
+  }
+
+  public down(): void {
+    this.blocks = this.blocks.map(b => {
+      b.point.y++
+      return b
+    })
+  }
+
+  public left(): void {
+    this.blocks = this.blocks.map(b => {
+      b.point.x--
+      return b
+    })
+  }
+
+}
+
+class Square extends Tetrimino implements ITetrimino {
+  constructor(p: Point) {
+    const blocks: Block[] = [
+      new Block(p),
+      new Block(new Point(p.x + 1, p.y)),
+      new Block(new Point(p.x, p.y + 1)),
+      new Block(new Point(p.x + 1, p.y + 1)),
+    ]
+    super(blocks)
+  }
 }
 
 export default Vue.extend({
@@ -40,36 +103,49 @@ export default Vue.extend({
   data() {
     return {
       tetriminos: [] as Tetrimino[],
+      activeTetrimino: {} as Tetrimino
     }
   },
   created(): void {
     this.addTetrimino()
+    this.addEventListeners()
+  },
+  beforeDestroy(): void {
+    this.removeEventListeners()
   },
   methods: {
     addTetrimino(): void {
-      const blocks: Block[] = [
-        { x: 0, y: 5 },
-        { x: 0, y: 6 },
-        { x: 1, y: 5 },
-        { x: 1, y: 6 },
-      ]
-      const tetrimino = { blocks: blocks } as Tetrimino
-      this.tetriminos.push(tetrimino)
-      console.log(this.tetriminos)
+      const tetrimino: Tetrimino = new Square(new Point(5, 0)) 
+      this.activeTetrimino = tetrimino
     },
     hasBlock(x: number, y: number): boolean {
-      const ret = this.tetriminos.some(t => {
-        return t.blocks.some(b => b.x === x && b.y === y)
-      })
-      console.log(x, y, ret)
-      return ret
+      return this.tetriminos.some(t => t.hasBlock(x, y))
     },
     getColor(x: number, y: number): string {
       if (this.hasBlock(x, y)) {
         return 'black'
+      } else if (this.activeTetrimino != null && this.activeTetrimino.hasBlock(x, y)) {
+        return 'gray'
       }
       return 'white'
-    }
+    },
+    addEventListeners(): void {
+      window.addEventListener('keyup', this.handleKeyUpArrows)
+    },
+    removeEventListeners(): void {
+      window.removeEventListener('keyup', this.handleKeyUpArrows)
+    },
+    handleKeyUpArrows(event: KeyboardEvent): void {
+      if(event.code === 'ArrowLeft') {
+        this.activeTetrimino.left()
+      }
+      if(event.code === 'ArrowDown') { 
+        this.activeTetrimino.down()
+      }
+      if(event.code === 'ArrowRight'){ 
+        this.activeTetrimino.right()
+      }
+    },
   },
 })
 </script>
